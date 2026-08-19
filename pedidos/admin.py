5146e54da71786a7478dc8f187e0656d58843b93
Karla Corona <km.lopez005@ebc.edu.mx>
2026-08-18 21:47:47 -0600

    Actualización 18 agosto

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Producto, Pedido
 
+admin.site.site_header = "Admin Cafetería Karla"
+admin.site.site_title = "Panel Cafetería Karla"
+admin.site.index_title = "Control de operaciones"
+
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'precio', 'categoria', 'disponible')
