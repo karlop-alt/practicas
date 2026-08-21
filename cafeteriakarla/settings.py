0e33c2a9fa3a1c4c22294544a3bb2fea99606d9e
Karla Corona <km.lopez005@ebc.edu.mx>
2026-08-20 20:31:19 -0600

    update de seguridad 20 agosto

--- cafeteriakarla/settings.py
+++ cafeteriakarla/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #Middleware personalizado para auditoría de seguridad
+    'pedidos.middleware.AuditoriaSeguridadMiddleware',
 ]
 
 ROOT_URLCONF = 'cafeteriakarla.urls'
@@ -122,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+#configuraciones de los archivos de media file
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email
